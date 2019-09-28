517a8f0f48f0c178a5f77711c2e3368fb633d960
Tess Gronski <theresegronski@Tesss-MacBook-Air.local>
2019-09-28 12:02:31 -0700

    first commit

---
+++ setupTests.js
@@ -0,0 +1,6 @@
+ lines (3 sloc)  120 Bytes
+  
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
---
+++ someOfMyfiles/notesList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import React, { Component } from 'react';
+
+
+class NotesList extends React.Component {
+//   static defaultProps = {
+//     bookmarks: []
+//   };
+
+  render() {
+    // const { bookmarks } = this.props
+    return (
+      <section className='NotesList'>
+        {/* <h2>Your bookmarks</h2>
+        <ul className='BookmarkList__list' aria-live='polite'>
+          {bookmarks.map(bookmark =>
+            <BookmarkItem
+              key={bookmark.id}
+              {...bookmark}
+            />
+          )}
+        </ul> */}
+        <p>hi</p>
+      </section>
+    );
+  }
+}
+
+export default NotesList;
